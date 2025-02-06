fa04a5aad6461b9b393d3f39e0ec2f3f042437a0
Tony Hsu <tonyhsu17@users.noreply.github.com>
2025-02-06 00:39:09 -0500

    Added long args

--- src/main/java/org/tonyhsu17/Params.java
+++ src/main/java/org/tonyhsu17/Params.java
@@ -10,12 +10,12 @@ import org.tonyhsu17.utilities.commandline.Parameter;
  * @author Tony Hsu
  */
 public class Params {
-    public static final Parameter S = new Parameter("s", true, "source path");
-    public static final Parameter D = new Parameter("d", true, "destination path");
-    public static final Parameter U = new Parameter("u", true, "https://www.shanaproject.com/USERNAME");
-    public static final Parameter T = new Parameter("t", true, "cron time in sec");
+    public static final Parameter S = new Parameter("s", "src", true, "source path");
+    public static final Parameter D = new Parameter("d", "des", true, "destination path");
+    public static final Parameter U = new Parameter("u", "url", true, "https://www.shanaproject.com/USERNAME");
+    public static final Parameter T = new Parameter("t", "time", true, "cron time in sec");
     public static final Parameter VERBOSE = new Parameter("v", "verbose", false, "output logging");
-    public static final Parameter ONCE = new Parameter("once", false, "run single time");
+    public static final Parameter ONCE = new Parameter("once", "once", false, "run single time");
     public static final Parameter SIZE = new Parameter("l", "logSize", true, "max history size");
 
     public static Parameter[] params = {S, D, U, T, VERBOSE, ONCE, SIZE};
